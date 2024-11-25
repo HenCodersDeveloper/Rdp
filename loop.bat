bc776783b3c5571385f08cc989927c3e5bcc373b
HenCoders <lincolncowper@outlook.com>
2024-11-26 04:33:34 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,8 +1,8 @@
 @echo off
 echo RDP CREATION SUCCESSFUL!
-tasklist | find /i "loclx.exe" >Nul && goto check || echo "Unable to get LocalXpose tunnel, make sure LocalXpose authentication is correct in Settings > Secrets > Repository Secret. Maybe your previous VM is still running." & ping 127.0.0.1 >Nul & exit
+tasklist | find /i "ssh.exe" >nul && goto check || echo "Unable to get Serveo tunnel. Please ensure the workflow logs show a valid Serveo subdomain." & ping 127.0.0.1 >nul & exit
 :check
-ping 127.0.0.1 > null
+ping 127.0.0.1 >nul
 cls
 echo RDP CREATION SUCCESSFUL!
 goto check
